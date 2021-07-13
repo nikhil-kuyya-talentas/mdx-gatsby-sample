71b4acabe86ad481c871bf4342ba525f54f98202
Nikhil Kuyya <nikhil.k@talentas.in>
2021-07-13 13:47:10 +0530

    clean styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,7 @@
 import * as React from "react"
 import Layout from '../components/layout'
-// styles
-// const pageStyles = {
-//   color: "#232129",
-//   padding: 96,
-//   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-// }
 
-// const headingStyles = {
-//   marginTop: 0,
-//   marginBottom: 64,
-// }
+// styles
 
 
 // markup
