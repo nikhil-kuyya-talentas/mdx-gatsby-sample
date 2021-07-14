0896884dcaa3230fc5e833d4d8529c6281fd645c
Nikhil Kuyya <nikhil.k@talentas.in>
2021-07-14 18:19:40 +0530

    comment test forms

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,30 @@ import Layout from '../components/layout';
 const Aboutpage = () => {
     return <Layout>
         <h1>About Page</h1>
+        <form name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true">
+            <p>
+                <label>Your Name: <input type="text" name="name" /></label>
+            </p>
+            <p>
+                <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+                <label>Your Role:
+                    <select name="role[]" multiple>
+                        <option value="leader">Leader</option>
+                        <option value="follower">Follower</option>
+                    </select>
+                </label>
+            </p>
+            <p>
+                <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <div data-netlify-recaptcha="true"></div>
+
+            <p>
+                <button type="submit">Send</button>
+            </p>
+        </form>
     </Layout>
 }
 export default Aboutpage;
