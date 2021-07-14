8a8c61ed8894706abcf8a0b1eaa09c43f054a22c
Nikhil Kuyya <nikhil.k@talentas.in>
2021-07-14 11:13:29 +0530

    add changes in folder structure and mdx as page

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,19 @@ module.exports = {
   siteMetadata: {
     siteUrl: "https://determined-swirles-11060f.netlify.app",
     title: "mdx-explore-gatsby",
+    description : "Exploring Netlify CMS with gatsby"
   },
   plugins: [
     "gatsby-plugin-emotion",
-    "gatsby-plugin-netlify-cms"
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-mdx",
+      options : {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js')
+        }
+      }
+    }
   ],
 };
