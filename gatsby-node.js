d44c167604621ccdfc07cd300bdb7a526f42a909
Nikhil Kuyya <nikhil.k@talentas.in>
2021-07-15 14:12:51 +0530

    test

---
+++ gatsby-node.js
@@ -0,0 +1,21 @@
+const { createFilePath } = require("gatsby-source-filesystem")
+
+exports.onCreateWebpackConfig = ({ actions }) => {
+    actions.setWebpackConfig({
+        devtool: "eval-source-map"
+    });
+};
+
+exports.onCreateNode = args => {
+  console.log(args)
+  const { actions, node } = args
+  if (node.internal.type === "MarkdownRemark") {
+    const { createNodeField } = actions
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,9 +2,25 @@ import * as React from 'react';
 import Layout from '../components/layout';
 
 const Aboutpage = () => {
+    const formElement = React.useRef(null);
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let formData = new FormData(formElement.current)
+        fetch('/contact', {
+          method: 'POST',
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: new URLSearchParams(formData).toString()
+        }).then(() => console.log('Form successfully submitted')).catch((error) =>
+          alert(error))
+      }
+      
+
+
     return <Layout>
         <h1>About Page</h1>
-        <form name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true">
+        <form name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true" netlify-honeypot="bot-field" ref={formElement}>
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="form-name" value="contact" />
             <p>
                 <label>Your Name: <input type="text" name="name" /></label>
             </p>
@@ -25,7 +41,7 @@ const Aboutpage = () => {
             <div data-netlify-recaptcha="true"></div>
 
             <p>
-                <button type="submit">Send</button>
+                <button type="submit" onClick={handleSubmit}>Send</button>
             </p>
         </form>
     </Layout>
